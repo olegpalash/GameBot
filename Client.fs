f1fe95092c6080776e177207b6a5dc3801710cb1
Oleg Palash <oleg.palash.98@gmail.com>
2022-01-29 21:27:31 +0200

    Add class GameBot.Client

---
+++ Client.fs
@@ -0,0 +1,28 @@
+namespace GameBot
+
+open System
+open System.Net
+open System.Net.Http
+
+open System.Collections.Generic
+
+type Client(address: string) = 
+    let cookies = new CookieContainer()
+    let handler = new SocketsHttpHandler()
+    do handler.CookieContainer <- cookies
+
+    let client = new HttpClient(handler)
+    let baseUri = new Uri(address)
+    do client.BaseAddress <- baseUri
+
+    member this.Get(path: string) = 
+        client.GetAsync path
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    member this.Post(path: string, data) = 
+        let formData = new FormUrlEncodedContent(data)
+
+        client.PostAsync(path, formData)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
